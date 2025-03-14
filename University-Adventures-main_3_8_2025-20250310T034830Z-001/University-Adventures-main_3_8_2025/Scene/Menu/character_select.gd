edfec44811275b276395aeb1e88296e51578d414
wengell2403 <wengrqn@gmail.com>
2025-03-15 01:21:50 +0800

    March 15 2025
    bshm level up to 7

--- University-Adventures-main_3_8_2025-20250310T034830Z-001/University-Adventures-main_3_8_2025/Scene/Menu/character_select.gd
+++ University-Adventures-main_3_8_2025-20250310T034830Z-001/University-Adventures-main_3_8_2025/Scene/Menu/character_select.gd
@@ -3,3 +3,11 @@ extends Node
 
 func _on_level_1_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scene/Levels/level1.tscn")
+
+
+func _on_level_1_bshm_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/Levels/level1_bshm.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
